0a54122a5eb68d8911461233b02967325db90d3c
Sai-Harshith-315 <saiharshith.vanguard@gmail.com>
2024-10-14 15:45:51 +0530

    updated code
    modified the bindings file with the firebaseAuthService and Interfaces

--- lib/bindings/bindings.dart
+++ lib/bindings/bindings.dart
@@ -1,13 +1,14 @@
 import 'package:get/get.dart';
 import 'package:grocery_user_app/controllers/signup_controller.dart';
+import '../data/interfaces/interfaces.dart';
 import '../data/services/firebase_auth_repo.dart';
 
 class AppBindings extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<FirebaseAuthServices>(() => FirebaseAuthServices());
+    Get.lazyPut<Interfaces>(() => FirebaseAuthServices());
 
     Get.lazyPut<SignupController>(
-        () => SignupController(interfaces: Get.find<FirebaseAuthServices>()));
+        () => SignupController(interfaces: Get.find<Interfaces>()));
   }
 }
